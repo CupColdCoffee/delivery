01b2fe32cba5c48540a8814a89d4f1dd2fc6cf7c
CupColdCoffee <aartfaan@mail.ru>
2020-06-26 16:46:11 +0400

    i think, all complete, above php

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp        = require('gulp');
 const browserSync = require('browser-sync');
 const sass        = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
-// const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
@@ -11,7 +11,7 @@ gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 
@@ -22,49 +22,47 @@ gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
-        // .pipe(autoprefixer())
+        .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
-    // gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
 });
 
-// gulp.task('html', function () {
-//     return gulp.src("src/*.html")
-//         .pipe(htmlmin({ collapseWhitespace: true }))
-//         .pipe(gulp.dest("dist/"));
-// });
+gulp.task('html', function () {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist"));
+});
 
-// gulp.task('scripts', function () {
-//     return gulp.src("src/js/**/*.js")
-//         .pipe(gulp.dest("dist/js"));
-// });
+gulp.task('scripts', function () {
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"));
+});
 
-// gulp.task('fonts', function () {
-//     return gulp.src("src/fonts/**/*")
-//         .pipe(gulp.dest("dist/fonts"));
-// });
+gulp.task('fonts', function () {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"));
+});
 
-// gulp.task('icons', function () {
-//     return gulp.src("src/icons/**/*")
-//         .pipe(gulp.dest("dist/icons"));
-// });
+gulp.task('icons', function () {
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest("dist/icons"));
+});
 
-// gulp.task('mailer', function () {
-//     return gulp.src("src/mailer/**/*")
-//         .pipe(gulp.dest("dist/mailer"));
-// });
+gulp.task('mailer', function () {
+    return gulp.src("src/mailer/**/*")
+        .pipe(gulp.dest("dist/mailer"));
+});
 
-// gulp.task('images', function () {
-//     return gulp.src("src/img/**/*")
-//         .pipe(imagemin())
-//         .pipe(gulp.dest("dist/img"));
-// });
+gulp.task('images', function () {
+    return gulp.src("src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"));
+});
 
-// 'scripts', 'fonts', 'icons', 'mailer', 'html', 'images'
-
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'mailer', 'images'));
--- src/js/script.js
+++ src/js/script.js
@@ -9,4 +9,48 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_prev.png" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_next.png" alt=""></button>'
     });
+
+    function validateForms (form) {
+        $(form).validate ({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: "required"
+            },
+            messages: {
+                name: false,
+                phone: false,
+                email: {
+                    required: false,
+                    email: false
+                },
+                message: false
+            }
+        });
+    }
+
+    validateForms('#questions-form'); 
+    validateForms('#consultation-form'); 
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+      
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1400) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+
 });
